21f209b66215a5a2095cd903b135d96b91154c32
yasminlopes <yclopes27@gmail.com>
2022-06-13 19:50:42 -0300

    vinculado ao front

--- script.js
+++ script.js
@@ -3,43 +3,98 @@
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
 
-const closeModal = () =>
+const closeModal = () => {
   document.getElementById("modal").classList.remove("active");
 
-const tempClient = {
-  nome: "Maria Paula",
-  email: "bipolar@email.com",
-  celular: "14999999990",
-  cidade: "Marília",
+  clearFields();
 };
 
-const getLocaStorage = () =>
-  JSON.parse(localStorage.getItem("db_client")) ?? [];
+/*const tempClient = {
+  categoria: "Televisão",
+  nome: "TV Smart Samsung",
+  preco: "2500",
+  Quantidade: "50",
+};*/
 
-const setLocalStorage = (db_client) =>
-  localStorage.setItem("db_client", JSON.stringify(db_client));
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("db_produto")) ?? [];
+
+const setLocalStorage = (db_produto) =>
+  localStorage.setItem("db_produto", JSON.stringify(db_produto));
 
 //CRUD
 /* Create (Envia os Dados)*/
-const createClient = (client) => {
-  const db_client = getLocaStorage();
-  db_client.push(client);
-  setLocalStorage(db_client);
+const createProduct = (produto) => {
+  const db_produto = getLocalStorage();
+  db_produto.push(produto);
+  setLocalStorage(db_produto);
 };
 /* Read */
-const readClient = () => getLocaStorage();
+const readProduct = () => getLocalStorage();
 /* Update */
-const updateClient = (index, client) => {
-  const db_client = readClient();
-  db_client[index] = client;
-  setLocalStorage(db_client);
+const updateProduct = (index, produto) => {
+  const db_produto = readProduct();
+  db_produto[index] = produto;
+  setLocalStorage(db_produto);
 };
 /* Delete */
-const deleteClient = (index) => {
-  const db_client = readClient();
-  db_client.splice(index, 1);
-  setLocalStorage(db_client); // Manda para o banco
+const deleteProduct = (index) => {
+  const db_produto = readProduct();
+  db_produto.splice(index, 1);
+  setLocalStorage(db_produto); // Manda para o banco
 };
+
+// Vinculando com o Front
+/* Verifica se os itens obrigatórios estão preenchidos */
+const isValidFields = () => {
+  return document.getElementById("form").reportValidity();
+};
+/* Limpando os campos do form após o cadastro*/
+const clearFields = () => {
+  const camposForm = document.querySelectorAll(".modal-field");
+  camposForm.forEach((camposForm) => (camposForm.value = ""));
+};
+/* Dados preenchidos do form sendo enviados ao banco de dados atráves do ID*/
+const saveProduct = () => {
+  if (isValidFields()) {
+    const produto = {
+      categoria: document.getElementById("categoria").value,
+      nome: document.getElementById("nome").value,
+      preco: document.getElementById("preco").value,
+      quantidade: document.getElementById("quantidade").value,
+    };
+    createProduct(produto);
+    console.log("[INFO] Produto Cadastrado com sucesso!");
+    closeModal();
+  }
+};
+// Trazendo dados preenchidos a tabela do Front
+const createRow = (produto) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+      <td>${produto.categoria}</td>
+      <td>${produto.nome}</td>
+      <td>R$ ${produto.preco}</td>
+      <td>${produto.quantidade}</td>
+      <td>
+          <button type="button" class="button green">Editar</button>
+          <button type="button" class="button red" >Excluir</button>
+      </td>
+  `;
+  document.querySelector("#tableProdutos>tbody").appendChild(newRow);
+};
+// Apagando os Dados da Tabela
+const clearTable = () => {
+  const rows = document.querySelectorAll("#tableProdutos>tbody tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
+};
+//Atualizando
+const updateTable = () => {
+  const db_produto = readProduct();
+  clearTable();
+  db_produto.forEach(createRow);
+};
+updateTable();
 
 // Eventos
 document
@@ -47,3 +102,4 @@ document
   .addEventListener("click", openModal);
 
 document.getElementById("modalClose").addEventListener("click", closeModal);
+document.getElementById("btnSalvar").addEventListener("click", saveProduct);
